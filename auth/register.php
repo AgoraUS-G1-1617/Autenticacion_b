4d3dc531afd94074021eb5c2242b0d018369adb1
Rubén Suárez Almenta <ruben_suarez1995@hotmail.com>
2016-11-30 13:35:07 +0100

    Eliminado el captcha para que funcione el register.php

--- auth/register.php
+++ auth/register.php
@@ -7,29 +7,11 @@
 * \author auth.agoraUS
 */
 
-require_once 'captcha/ReCaptcha.php';
-require_once 'captcha/RequestMethod.php';
-require_once 'captcha/RequestParameters.php';
-require_once 'captcha/Response.php';
-require_once 'captcha/RequestMethod/Post.php';
-require_once 'captcha/RequestMethod/Socket.php';
-require_once 'captcha/RequestMethod/SocketPost.php';
 
 $clave_del_sitio = "6LfD6hcTAAAAAOLQVRMu_oJA4eCRIUxGj0tAo8HJ";
 $clave_secreta = "6LfD6hcTAAAAALJdSU9xW9qZfDy0PkvcJLPs7HE4";
 
-if($_POST['accion'] == 'enviar'){
-    $recaptcha = new ReCaptcha($clave_secreta);
-    $respuesta = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-    if($respuesta->isSuccess()){
-        echo 'El formulario ha sido validado';
-    }else{
-        echo 'Se ha devuelto el siguiente error:';
-        foreach ($respuesta->getErrorCodes() as $error_code) {
-            echo '<tt>' . $error_code . '</tt> ';
-        }
-    }
-}
+
 
 include_once("database.php");
 session_start();
@@ -40,7 +22,6 @@ session_start();
    <meta charset="utf-8">
    <link rel="stylesheet" href="layout.css" />
    <script src="lib/jquery-2.1.1.min.js"></script>
-   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
    
    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 	<script type="text/javascript" src="bootstrap/js/bootstrap.mi.js"></script>
@@ -266,7 +247,6 @@ session_start();
 <br />
 <br />
                 
-                <div class="g-recaptcha" data-sitekey="6LfD6hcTAAAAAOLQVRMu_oJA4eCRIUxGj0tAo8HJ" align="center"></div>
                 <div align="center">  
                 	<input  type="submit" 
                             id="submit" 

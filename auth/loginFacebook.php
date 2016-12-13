1e0e4a68ab6dd5898bdd7effb36c8eda674289ad
Alejandro Rodríguez <alerodri1994@gmail.com>
2016-12-13 13:59:33 +0100

    Cambio identificador aplicación Facebook

--- auth/loginFacebook.php
+++ auth/loginFacebook.php
@@ -12,7 +12,7 @@ $fb = new Facebook\Facebook([
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('fb-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('../fb-callback.php', $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 

f1f896362a2aa0bcac0e6d7406b9cc2c80d9430c
Carlos Campos <carcamcue@gmail.com>
2016-12-21 13:20:16 +0100

    Cambio API Facebook id

--- auth/loginFacebook.php
+++ auth/loginFacebook.php
@@ -4,7 +4,7 @@ session_start();
 require_once "Facebook/autoload.php";
 
 $fb = new Facebook\Facebook([
-  'app_id' => '1166797976702364', // Replace {app-id} with your app id
+  'app_id' => '1166797976702360', // 4 Replace {app-id} with your app id
   'app_secret' => '21a7d98d0f739cef31c6dc5f43f37716',
   'default_graph_version' => 'v2.8',
   ]);

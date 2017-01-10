497baa97f6d9398e965300266ac1f4419531a55a
alerodrom <alerodri1994@gmail.com>
2017-01-10 11:31:44 +0100

    Cambio app Id Twitter y Facebook

--- auth/configTwitter.php
+++ auth/configTwitter.php
@@ -8,10 +8,4 @@ return [
     
     'url_login'         => 'https://authb.agoraus1.egc.duckdns.org/twitter_login.php',
     'url_callback'      => 'https://authb.agoraus1.egc.duckdns.org/twitter_callback.php',
-    /* //Develop 
-    'consumer_key'      => 'kOtCYqQszZOuznCGdq4r7X6tE',
-    'consumer_secret'   => 'h0e1cR7iBbQdSnoYHV8pORi5PXlqRnwhbWJUAWuDC7HmxInjzs',
- 
-    'url_login'         => 'https://beta.authb.agoraus1.egc.duckdns.org/twitter_login.php',
-    'url_callback'      => 'https://beta.authb.agoraus1.egc.duckdns.org/twitter_callback.php',*/
 ];

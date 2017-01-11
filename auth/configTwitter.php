2eda9f8438a07a9d01a09f996276a2985bdd4258
Alejandro Rodríguez <alerodri1994@gmail.com>
2017-01-11 16:00:12 +0100

    Preparando appId para facebook

--- auth/configTwitter.php
+++ auth/configTwitter.php
@@ -1,11 +1,17 @@
 <?php
  
 return [
+	
+	//Beta
+	
+	// key: 	kOtCYqQszZOuznCGdq4r7X6tE
+	// secret:	h0e1cR7iBbQdSnoYHV8pORi5PXlqRnwhbWJUAWuDC7HmxInjzs
+
     //Master
     'consumer_key'      => 'FjQNkpO9y27lK2SYUaSIhXg6f',
     'consumer_secret'   => 'vpe7rmsSeuNw7eHeLYrfUOgQjNRVMf6ksAX9fdpS9RncqlPb1T',
  
     
-    'url_login'         => 'https://authb.agoraus1.egc.duckdns.org/twitter_login.php',
-    'url_callback'      => 'https://authb.agoraus1.egc.duckdns.org/twitter_callback.php',
+    'url_login'         => 'https://beta.authb.agoraus1.egc.duckdns.org/twitter_login.php',
+    'url_callback'      => 'https://beta.authb.agoraus1.egc.duckdns.org/twitter_callback.php',
 ];
